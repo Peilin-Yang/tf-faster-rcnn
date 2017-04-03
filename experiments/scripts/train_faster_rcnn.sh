ada619cbf89c7545347117b3273358cba4729e5a
Peilin Yang <yangpeilyn@gmail.com>
2017-04-03 07:34:35 -0400

    add bib to experiment scripts

--- experiments/scripts/train_faster_rcnn.sh
+++ experiments/scripts/train_faster_rcnn.sh
@@ -15,6 +15,12 @@ EXTRA_ARGS=${array[@]:3:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
 case ${DATASET} in
+  bib)
+    TRAIN_IMDB="bib_500X500_training"
+    TEST_IMDB="bib_500X500_testing"
+    PT_DIR="bib"
+    ITERS=70000
+    ;;
   pascal_voc)
     TRAIN_IMDB="voc_2007_trainval"
     TEST_IMDB="voc_2007_test"
