083cc12ece774a1c52ed795f9b7c7f9b3f0b8e1a
Peilin Yang <yangpeilyn@gmail.com>
2017-04-04 11:33:44 -0400

    add detection

--- tools/detect_net.py
+++ tools/detect_net.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
     print(('Loading model check point from {:s}').format(args.model))
     print([v.name for v in tf.global_variables()])
-    print(args.model)
+    print(len(args.model), args.model)
     faster_rcnn_vars = [v.name for v in tf.global_variables() 
                           if v.name.startswith(args.faster_rcnn_net)]
     faster_rcnn_saver = tf.train.Saver(faster_rcnn_vars)
