86307886729c0fa0ff61e1666e720069ca0eb693
Peilin Yang <yangpeilyn@gmail.com>
2017-04-05 11:16:27 -0400

    merge overlapping rectangle

--- lib/model/detect.py
+++ lib/model/detect.py
@@ -169,7 +169,7 @@ def _remove_overlapping_boxes(boxes):
   return clusters
 
 boxes = [[10, 10, 50, 70], [8, 10, 30, 20], [90, 90, 99, 99]]
-print _remove_overlapping_boxes(boxes)
+print(_remove_overlapping_boxes(boxes))
 
 def im_detect(sess, net, im):
   blobs, im_scales = _get_blobs(im)
